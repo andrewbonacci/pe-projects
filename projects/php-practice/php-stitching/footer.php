8be17664fd0611706514fd909dca6308a15fe917
Andrew <abonacci@protonmail.com>
2022-02-12 21:33:48 -0600

    PHP Practice Files
    PHP Practice Files

---
+++ projects/php-practice/php-stitching/footer.php
@@ -0,0 +1,13 @@
+		<!--CONTENT HERE-->
+		</main>
+
+		<footer>
+			<div class="inner-column">
+			<h2>PHP stitching (footer)</h2>
+		</div>
+
+		</footer>
+
+	</body>
+
+</html>
---
+++ projects/php-practice/php-stitching/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang='en'>
+
+	<head>
+		<title>PHP Stitching</title>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<meta name='description' description='[[ insert description ]]'>
+		<meta property='og:image' content='images/default-meta.png'>
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
+
+	<body>
+
+		<header>
+			<div class="inner-column">
+				<h1>This is PHP stitching (header)</h1>
+				<nav></nav>
+			</div>
+		</header>
+
+		<main>
+		<!--CONTENT HERE-->
+	
