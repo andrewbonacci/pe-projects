5bcf72915ef0ebd3f43a2add911ff07064c447d4
Andrew <abonacci@protonmail.com>
2022-04-17 19:09:59 -0500

    Updated breakpoints, <wbr> and font sizes

--- index.php
+++ index.php
@@ -13,9 +13,15 @@
 	<body>
 
 	<header>
-		<h1>Hello! My name is <span>Andrew Bonacci</span>. My website is being redesigned.</h1>
-		<h2>For questions or inquires regarding web development and consulting, please reach me at <span>abonacci@protonmail.com</span>.
+		<div class='inner-column'>
+		<h1>Hello! My name is <span>Andrew Bonacci</span>.
+		<br>
+		My website is being redesigned.</h1>
+		<h2>For questions or inquires regarding 
+		<br>web development and consulting, <wbr>
+		please reach me at <span>abonacci@protonmail.com</span>.</h2>
 		<h2>Thank you!</h2>
+	</div>
 	</header>
 	
 	</body>
