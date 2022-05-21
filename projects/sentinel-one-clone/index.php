c5337a6801999c11f7c2a3ff7fea0d6c3a5dbff3
Andrew <abonacci@protonmail.com>
2022-05-20 20:47:05 -0500

    Sentinel One Clone Initial File Upload

---
+++ projects/sentinel-one-clone/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang='en'>
+
+	<head>
+		<title>[[ placeholder ]]</title>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<meta name='description' description='[[ insert description ]]'>
+		<meta property='og:image' content='images/default-meta.png'>
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
+
+	<body>
+
+<!-- use inner-column as needed -->
+
+		<header>
+			<div class="inner-column">
+				<h1>[[ placeholder ]]</h1>
+				<nav></nav>
+			</div>
+		</header>
+
+		<main>
+			<section></section>
+		</main>
+
+		<footer>
+			<nav></nav>
+		</footer>
+
+	</body>
+
+</html>
