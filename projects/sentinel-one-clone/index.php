6e91970d22d45d890e5f869019bf940130e65586
Andrew <abonacci@protonmail.com>
2022-05-23 22:55:11 -0500

    Sentinel One HTML Upload (Draft)
    Completed draft of Sentinel One HTML. This will be edited as needed.

--- projects/sentinel-one-clone/index.php
+++ projects/sentinel-one-clone/index.php
@@ -2,33 +2,453 @@
 <html lang='en'>
 
 	<head>
-		<title>[[ placeholder ]]</title>
+		<title>SentinelOne Clone</title>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
-		<meta name='description' description='[[ insert description ]]'>
-		<meta property='og:image' content='images/default-meta.png'>
+		<meta name='description' description='SentinelOne Clone'>
+		<meta property='og:image' content=''>
 		<link rel='stylesheet' href='css/style.css'>
 	</head>
 
 	<body>
-
-<!-- use inner-column as needed -->
-
+<!-- SentinelOne Header -->
 		<header>
-			<div class="inner-column">
-				<h1>[[ placeholder ]]</h1>
-				<nav></nav>
-			</div>
+			<inner-column>
+
+				<nav>
+					<picture>
+						<img><!-- SentinelOne Logo -->
+					</picture>
+					<div class="lanugage-picker"></div>
+					<div class="menu-hamgburger-icon"></div>
+				</nav>
+
+			</inner-column>
 		</header>
 
+
+
+
 		<main>
-			<section></section>
+		<!-- SentinelOne Intro Section -->
+			<section class="intro">
+				<inner-column>
+					<h1>The Age of More is Over</h1>
+					<h2>Welcome to Singularity XDR</h2>
+					<p>One platform. Unprecedented speed. Infinite scale. The future of cybersecurity is autonomous. Singularity XDR secures endpoint, cloud, and identity.</p>
+					<button>Get Started</button>
+				</inner-column>
+			</section>
+
+		
+
+
+		<!-- SentinelOne News Section -->
+			<section class="news">
+				<inner-column>
+
+					<news-card>
+						<h1>NEWS</h1>
+						<h2>SentinelOne Completes Acquisition of Attivo Networks</h2>
+						<button>Learn More</button>
+					</news-card>
+
+					<news-card>
+						<h1>NEWS</h1>
+						<h2>SentinelOne Named a Leader in the 2021 Gartner Magic Quadrant for Endpoint Protection Programs</h2>
+						<button>Learn More</button>
+					</news-card>
+
+					<news-card>
+						<h1>NEWS</h1>
+						<h2>#1 Again. SentinelOne Leads in the Latest MITRE ATT&CK Evaluation with 100% Prevention.</h2>
+						<button>Learn More</button>
+					</news-card>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Exploration Section -->
+			<section class="exploration">
+				<inner-column>
+
+					<h1>The Future of Cybersecurity is Autonomous</h1>
+					<p>Today’s cyber attackers move fast. Fast enough that 1-10-60 has become an obsolete model for effective detection, investigation, and response. Singularity XDR is the only cybersecurity platform empowering modern enterprises to take action in real-time with greater visibility of their dynamic attack surface with AI-powered automation.</p>
+
+					<button>EXPLORE THE PLATFORM</button>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Cube Background Section (BG IMG ONLY) -->
+			<section class="cube-bg">
+			</section>
+
+
+
+
+		<!-- SentinelOne Singulary Platform Section -->
+			<section class="singularity">
+				<inner-column>
+
+					<picture>
+						<img>SINGULARIY PLATFORM LOGO
+					</picture>
+
+					<logo-grouping>
+						<picture>
+							<img>ENDPOINT LOGO
+						</picture>
+						<p>ENDPOINT</p>
+					</logo-grouping>
+
+					<logo-grouping>
+						<picture>
+							<img>CLOUD LOGO
+						</picture>
+						<p>CLOUD</p>
+					</logo-grouping>
+
+					<logo-grouping>
+						<picture>
+							<img>IOT LOGO
+						</picture>
+						<p>IOT</p>
+					</logo-grouping>
+
+					<logo-grouping>
+						<picture>
+							<img>DATA LOGO
+						</picture>
+						<p>DATA</p>
+					</logo-grouping>
+
+					<logo-grouping>
+						<picture>
+							<img>IDENTITY LOGO
+						</picture>
+						<p>IDENTITY</p>
+					</logo-grouping>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Innovcation Section -->
+			<section class="innovation">
+				<inner-column>
+
+					<h1>Innovation at the Core</h1>
+					<h2>We Are Pushing the Boundaries of Autonomous Technology. This Is How.</h2>
+
+					<innovation-cards>
+
+						<innov-card>
+							<picture>
+								<img><!-- XDR LOGO -->
+							</picture>
+							<h1>Actionable XDR</h1>
+							<p>Don’t stop at just identifying malicious behaviors. Block and remediate advanced attacks autonomously at machine speed with cross-platform, enterprise-scale data analytics.</p>
+						</innov-card>
+
+						<innov-card>
+							<picture>
+								<img><!-- AI LOGO -->
+							</picture>
+							<h1>Distributed AI</h1>
+							<p>Equip every endpoint and workload—no matter their location or connectivity—to respond intelligently against cyber threats with powerful static & behavioral AI.</p>
+						</innov-card>
+
+						<innov-card>
+							<picture>
+								<img><!-- STORYLINE LOGO -->
+							</picture>
+							<h1>Patented Storyline</h1>
+							<p>EEmpower analysts with the context they need, faster, by automatically connecting & correlating benign and malicious events in one illustrative view.</p>
+						</innov-card>
+
+					</innovation-cards>
+
+				</inner-column>
+			</section>
+
+
+
+		<!-- SentinelOne Breaches Section -->
+			<section class="breaches">
+				<inner-column>
+
+					<h1>Stopping Breaches Faster Than Any Human Could</h1>
+					<p>From endpoint to the cloud. From IoT device to the container. Data has become the foundation of our way of life and critical for organizations to protect. Harnessing its power at any moment in time is also the answer to defeating tomorrow’s evolving & emergent cyber threats. The SentinelOne Singularity Platform actions data at enterprise scale to make precise, context-driven decisions autonomously, at machine speed, without human intervention.</p>
+
+				</inner-column>
+			</section>
+
+
+
+		<!-- SentinelOne Logo Background Section BG IMG ONLY -->
+			<section class="logo-bg">
+			</section>
+
+
+
+
+		<!-- SentinelOne Customer Stories Section -->
+			<section class="stories">
+				<inner-column>
+
+					<h1>Securing the Best of the Best</h1>
+					<h2>3 of the Fortune 10 and Hundreds of the Global 2000</h2>
+					<p>At SentinelOne, customers are #1. Global industry leaders across every vertical thoroughly test and select us as their endpoint security solution of today and tomorrow. We protect trillions of dollars of enterprise value across millions of endpoints. The SentinelOne platform safeguards the world’s creativity, communications, and commerce on devices and in the cloud.</p>
+					<button>EXPLORE CUSTOMER STORIES</button>
+
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Customer Logo Section -->
+			<section class="customer-logos">
+				<inner-column>
+					<!-- REVISIT THIS LATER -->
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Cybersecurity Section -->
+			<section class="cybersecurity">
+				<inner-column>
+
+					<picture>
+						<img><!-- NORWEGIAN LOGO -->
+					</picture>
+					<h1>SentinelOne Has Changed the Way We Do Cybersecurity</h1>
+					<p>Tony Tuffe</p>
+					<p>IT Support Specialist</p>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Industry Section -->
+			<section class="industry">
+				<inner-column>
+
+					<h1>Back by the Industry</h1>
+					<h2>Tried and Trusted by the Industry's Leading Authorities, Analysts, and Associations.</h2>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Partners Section -->
+			<section class="partners">
+				<inner-column>
+
+					<partner-card>
+						<picture>
+							<img> <!-- Garner Logo -->
+						</picture>
+
+						<h1>ENDPOINT PROTECTION PLATFORMS</h1>
+						<ul>
+							<li>A Leader in the 2021 Magic Quadrant for Endpoint</li>
+							<li>Protection Platforms</li>
+						</ul>
+
+						<button>LEARN MORE</button>
+					</partner-card>
+
+					<partner-card>
+						<picture>
+							<img> <!-- Mitre Engenuity Logo -->
+						</picture>
+
+						<h1>RECORD BREAKING ATT&CK EVALUATION</h1>
+						<ul>
+							<li>100% Protection. 100% Detection.</li>
+							<li>Top Analytic Coverage 3 Years in a Row</li>
+							<li>100% Real-time with Zero Delays</li>
+						</ul>
+
+						<button>LEARN MORE</button>
+					</partner-card>
+
+					<partner-card>
+						<picture>
+							<img> <!-- peerinsights Logo -->
+						</picture>
+
+						<h1>99% OF EDR REVIEWERS RECOMMEND</h1>
+						<ul>
+							<li>4.9/5 Rating for Endpoint Protection Platforms and Endpoint Detection & Response Platforms</li>
+						</ul>
+
+						<button>LEARN MORE</button>
+					</partner-card>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- Sentinel "Power Of One" Section -->
+			<section class="power-of-one">
+				<inner-column>
+
+					<picture>
+						<img> <!-- OFFICIAL CYBERSECURITY PARTNERS IMAGE -->
+					</picture>
+
+					<h1>The Power of One</h1>
+					<p>SentinelOne is the Official Cybersecurity Partner of the Aston Martin Cognizant FormulaOne Team</p>
+
+					<button>LEARN MORE</button>
+
+				</inner-column>
+			</section>
+
+
+
+
+			<!-- INSIGHTS SECTION -->
+
+			<section class="insights">
+				<inner-column>
+
+					<picture>
+						<img> <!-- GOOD BAD UGLY IMG -->
+					</picture>
+
+					<insights-card>
+						<p>Company 4 | minute read</p>
+						<h1>The Good, The Bad and the Ugly in Cybersecurity - Week 21</h1>
+						<p>May 20, 2022</p>
+					</insights-card>
+
+					<insights-card>
+						<p>For CISO/CIO | 8 minute read</p>
+						<h1>How to Stay Ahead of the Adversary in 2022 | A Cybersecurity Checklist</h1>
+						<p>May 18, 2022</p>
+					</insights-card>
+
+					<insights-card>
+						<p>Identity | 4 minute read</p>
+						<h1>Denonia Malware Targets AWS Lambda Environments</h1>
+						<p>May 16, 2022</p>
+					</insights-card>
+
+					<insights-card>
+						<p>For CISO/CIO | 6 minute read</p>
+						<h1>Threat Landscape | The Most Dangerous Cloud Attack Methods In The Wild Today</h1>
+						<p>May 12, 2022</p>
+					</insights-card>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Purpose Section -->
+			<section class="purpose">
+				<inner-column>
+
+					<purpose-card>
+						<h1>Purpose Built to Prevent Tomorrow's Threats. Today.</h1>
+						<p>Your most sensitive data lives on the endpoint and in the cloud. Protect what matters most from cyberattacks. Fortify every edge of the network with realtime autonomous protection.</p>
+
+						<button>GET A DEMO</button>
+					</purpose-card>
+
+				</inner-column>
+			</section>
+
+
+
+
+		<!-- SentinelOne Footer -->
+			<footer>
+				<inner-column>
+
+					<h1>COMPANY</h1>
+
+					<a href="#">Our Customers</a>
+					<a href="#">Why SentinelOne</a>
+					<a href="#">Platform</a>
+					<a href="#">About</a>
+					<a href="#">Partners</a>
+					<a href="#">Support</a>
+					<a href="#">Careers</a>
+					<a href="#">Legal & Compliance</a>
+					<a href="#">Security & Compliance</a>
+					<a href="#">Contact Us</a>
+					<a href="#">Investor Relations</a>
+
+					<h1>RESOURCES</h1>
+
+					<a href="#">Blog</a>
+					<a href="#">Labs</a>
+					<a href="#">Hack Chat</a>
+					<a href="#">Press</a>
+					<a href="#">News</a>
+					<a href="#">FAQ</a>
+					<a href="#">Resources</a>
+
+					<h1>GLOBAL HEADQUARTERS</h1>
+
+					<p>444 Castro Street</p>
+					<p>Suite 400</p>
+					<p>Mountain View, CA 94041</p>
+					<p>+1-855-868-3733</p>
+					<p>sales@sentinelone.com</p>
+
+					<h1>SIGN UP FOR OUR NEWSLETTER</h1>
+					<input type="text" placeholder="Business Email">
+					<p>By clicking Subscribe, I agree to the use of my personal data in accordance with SentinelOne Privacy Policy. SentinelOne will not sell, trade, lease, or rent your personal data to third parties.</p>
+
+					<language-selector>
+						<picture>
+							<img><!--  GLOBE LOGO -->
+						</picture>
+					</language-selector>
+
+					<div class="purple-line"></div>
+
+					<p>@2022 SentinelOne, All Rights Reserved.</p>
+					<p>Privacy Policy</p>
+					<p>Terms of Service</p>
+
+					<picture>
+						<img> <!-- SENTINEL ONE LOGO -->
+					</picture>
+
+					<a href="#"><img> <!--  Twitter logo --></a>
+					<a href="#"><img> <!--  Facebook logo --></a>
+					<a href="#"><img> <!--  Linkedin logo --></a>
+					<a href="#"><img> <!--  YouTube logo --></a>
+
+				</inner-column>
+			</footer>
+
 		</main>
-
-		<footer>
-			<nav></nav>
-		</footer>
-
 	</body>
 
 </html>
